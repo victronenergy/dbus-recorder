b4989b0a2b01b88834ec05f712c1a33f503dd674
Jesus Perez <jperez@victronenergy.com>
2017-05-24 11:48:49 +0200

    Fix blocking on services with large number of items

--- dbusdevice.py
+++ dbusdevice.py
@@ -9,24 +9,31 @@ class DbusDevice(object):
 	# @param name the dbus-service-name.
 	def __init__(self, bus, name, eventCallback):
 		self._dbus_name = name
-		self._tree = Dbusitem(bus, name, '/')
-		self._value = {}
+		self._dbus_conn = bus
+		self._items = []
 		self._eventCallback = eventCallback
-		self._tree.foreach(self.add_item)
+		self._getChildren(bus, name)
+		self._service_id = self._dbus_conn.get_name_owner(name)
 
 	def __del__(self):
 		tracing.log.debug('__del__ %s' % self)
 		self._dbus_name = None
 		self._value = None
 		self._eventCallback = None
-		self._tree._delete()
 
-	def add_item(self, dbusitem):
-		tracing.log.debug(dbusitem.object.object_path)
-		self._value[dbusitem.object.object_path] = dbusitem
-		dbusitem._add_to_prop_changed()
-		dbusitem.SetEventCallback(self._eventCallback)
-		
+	def _getChildren(self, bus, service):
+		data = self._dbus_conn.call_blocking(service, '/', None, 'GetValue', '', [])
+		for child in data:
+			name = "/" + child
+			self._items.append(name)
+		self._dbus_conn.add_signal_receiver(self._on_dbus_value_changed,
+			dbus_interface='com.victronenergy.BusItem', signal_name='PropertiesChanged', path_keyword='path',
+			sender_keyword='service_id')
+
+	def _on_dbus_value_changed(self, changes, path=None, service_id=None):
+		if service_id != None and service_id == self._service_id:
+			self._eventCallback(self._dbus_name, path, changes)
+
 	## Returns the dbus-service-name which represents the Victron-device.
 	def __str__(self):
 		return "DbusDevice=%s" % self._dbus_name
@@ -36,13 +43,12 @@ class DbusDevice(object):
 	
 	def getValues(self):
 		values = {}
-		for obj_path, dbusitem in self._value.iteritems():
-			tracing.log.info("getValues %s" % obj_path)
+		for i in self._items:
 			properties = {}
-			properties['Value'] = dbusitem.value#dbusTypeToPythonType(obj_path, dbusitem.value)
-			properties['Valid'] = bool(dbusitem.valid)
-			properties['Text'] = str(dbusitem.text)
-			values[obj_path] = properties
+			properties['Value'] = self._dbus_conn.call_blocking(self._dbus_name, i, None, 'GetValue', '', [])
+			properties['Valid'] = bool(properties['Value'] != dbus.Array([]))
+			properties['Text'] = str(self._dbus_conn.call_blocking(self._dbus_name, i, None, 'GetText', '', []))
+			values[i] = properties
 		return values
 
 # convert to python type.
