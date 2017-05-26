c9fd66c39d5bb4a8351af457ab2b3de34bcd981c
Jesus Perez <jperez@victronenergy.com>
2017-05-26 10:57:50 +0200

    Add startdemo and stopdemo scripts

---
+++ startdemo.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+dir=$(dirname $0)
+
+# Stop services
+svc -d /service/serial-starter
+svc -d /service/vecan-mk2
+svc -d /service/vecan
+killall vedirect_dbus
+killall gps_dbus
+
+start() {
+	${dir}/dbusrecorder.py -p --file="${dir}/$1" &
+}
+
+# Start services
+start grid.dat
+start pvinverter.dat
+start solarcharger.dat
+start vebus.dat
+start battery.dat
