79b18a21335be75ac7be65b4cc25cc5957bfcb6d
mpvader <mvader@victronenergy.com>
2017-11-27 21:15:21 +0100

    play.sh & startdemo.sh: de-duplicate
    This make play.sh work again.

--- startdemo.sh
+++ startdemo.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-dir=$(dirname $0)
-
 # Stop services
 svc -d /service/serial-starter
 svc -d /service/vecan-mk2.$(basename $(cat /etc/venus/mkx_port))
@@ -9,16 +7,5 @@ svc -d /service/vecan
 killall vedirect_dbus
 killall gps_dbus
 
-start() {
-	${dir}/dbusrecorder.py -p --file="${dir}/$1" &
-}
-
-# Start services
-start solarcharger.dat
-start vebus.dat
-start battery-house.dat
-start battery-hydraulic.dat
-start tank_fwater.dat
-start tank_fuel.dat
-start tank_oil.dat
-start tank_bwater.dat
+# Start the demo
+$(dirname $0)/play.sh
