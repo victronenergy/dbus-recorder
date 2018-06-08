bc1654ad23d873ae1eaff51ffc913b0f2bcf1516
Jesus Perez <Jesus Miguel Perez Fernandez>
2018-06-08 09:38:38 +0000

    Update demo to ESS

--- play.sh
+++ play.sh
@@ -7,11 +7,8 @@ start() {
 }
 
 # Start services
+start grid.dat
+start pvinverter.dat
 start solarcharger.dat
 start vebus.dat
-start battery-house.dat
-start battery-hydraulic.dat
-start tank_fwater.dat
-start tank_fuel.dat
-start tank_oil.dat
-start tank_bwater.dat
+start battery.dat
