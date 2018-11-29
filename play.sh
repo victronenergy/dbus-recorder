d9bf0fb7894e33297dc7e67410bbf40640be4316
Izak Burger <isburger@gmail.com>
2018-11-29 13:12:38 +0200

    whitespace fix

--- play.sh
+++ play.sh
@@ -11,19 +11,19 @@ pgrep -f dbusrecorder.py | xargs -r kill -9
 
 # Start services
 case "$1" in
-	"2")
-		# Boat/Motorhome demo 1
-		start solarcharger.dat
-		start vebus.dat
-		start battery-house.dat
-		start battery-hydraulic.dat
-		start tank_fwater.dat
-		start tank_fuel.dat
-		start tank_oil.dat
-		start tank_bwater.dat
-		;;
-	"3")
-		# Boat/Motorhome demo 2
+    "2")
+        # Boat/Motorhome demo 1
+        start solarcharger.dat
+        start vebus.dat
+        start battery-house.dat
+        start battery-hydraulic.dat
+        start tank_fwater.dat
+        start tank_fuel.dat
+        start tank_oil.dat
+        start tank_bwater.dat
+        ;;
+    "3")
+        # Boat/Motorhome demo 2
         start solarcharger.dat
         start vebus-marine.dat
         start battery-marine.dat
@@ -32,7 +32,7 @@ case "$1" in
         start tank_oil.dat
         start tank_bwater.dat
         ;;
-	*)
+    *)
         # ESS demo
         start grid.dat
         start pvinverter.dat
