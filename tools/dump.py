9e4b1cfd9422a16ffa269c51f0334fb9171f115d
Izak Burger <isburger@gmail.com>
2021-10-05 16:02:26 +0200

    help dump tool to deal with python2 pickles
    Old pickles sometimes have UTF8String in it

--- tools/dump.py
+++ tools/dump.py
@@ -4,6 +4,15 @@ from csv import writer as csvwriter, excel_tab, QUOTE_NONE
 import pickle
 import json
 
+class Unpickler(pickle.Unpickler):
+    # Support for older pickles
+    _replacements = {
+        ('dbus', 'UTF8String'): ('dbus', 'String')
+    }
+    def find_class(self, module, name):
+        module, name = self._replacements.get((module, name), (module, name))
+        return super().find_class(module, name)
+
 class PropertiesChangedData(object):
     def __init__(self, time, dbusObjectPath, changes):
         self._time = time
@@ -48,18 +57,19 @@ def fmt_text(t):
 
 writer = csvwriter(sys.stdout, dialect=excel_tab, quotechar="'")
 with open(sys.argv[1], 'rb') as fp:
-    service = pickle.load(fp)
+    unpickler = Unpickler(fp, encoding='UTF-8')
+    service = unpickler.load()
     writer.writerow([service])
 
-    values = pickle.load(fp)
+    values = unpickler.load()
     for k, v in values.items():
-        writer.writerow([k, typeof(v['Value']), fmt(v['Value']), v['Text']])
+        writer.writerow([k, typeof(v['Value']), fmt(v['Value']), v['Text'].strip()])
     writer.writerow([])
 
     while True:
         try:
             #position = fp.tell()
-            change = pickle.load(fp)
+            change = unpickler.load()
             value = change._changes['Value']
 
             writer.writerow([change._time, change._dbusObjectPath, typeof(value), fmt(value), fmt_text(change._changes['Text'])])
