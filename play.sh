deaea14584558f36f0c4723020a422c2be01f8f7
Izak Burger <isburger@gmail.com>
2021-11-08 11:46:06 +0200

    add ruuvi temp sensor to boat/motorhome demos
    Most useable as a fridge.

--- play.sh
+++ play.sh
@@ -15,12 +15,13 @@ case "$1" in
         # Boat/Motorhome demo 1
         start solarcharger.csv vebus.csv battery-house.csv \
 			battery-hydraulic.csv tank_fwater.csv tank_fuel.csv \
-			tank_oil.csv tank_bwater.csv
+			tank_oil.csv tank_bwater.csv ruuvi.csv
         ;;
     "3")
         # Boat/Motorhome demo 2
         start solarcharger.csv vebus-marine.csv battery-marine.csv \
-			tank_fwater.csv tank_fuel.csv tank_oil.csv tank_bwater.csv
+			tank_fwater.csv tank_fuel.csv tank_oil.csv tank_bwater.csv \
+			ruuvi.csv
         ;;
     *)
         # ESS demo
